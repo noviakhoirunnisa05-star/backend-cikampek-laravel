c8ab8f6130b1a8f3ec1c4fe2c0cd35d432aab90b
lukitoadi <lukitoadi354@student.ub.ac.id>
2026-05-30 11:31:11 +0700

    melengkapi perubahan backend Laravel

--- app/Models/EducationLevel.php
+++ app/Models/EducationLevel.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EducationLevel extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'id_level';
+
+    protected $fillable = [
+        'nama_level',
+    ];
+
+    public function scholarships()
+    {
+        return $this->hasMany(Scholarship::class, 'id_level', 'id_level');
+    }
 }
--- app/Models/Major.php
+++ app/Models/Major.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Major extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'id_major';
+
+    protected $fillable = [
+        'nama_major',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'id_major', 'id_major');
+    }
+
+    public function scholarships()
+    {
+        return $this->belongsToMany(
+            Scholarship::class,
+            'scholarship_major',
+            'id_major',
+            'id_scholarship'
+        );
+    }
 }
