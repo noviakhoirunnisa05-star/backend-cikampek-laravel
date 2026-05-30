493c7d9966b155621fb9cdd509cd4130b45b4197
noviakhoirunnisa05-star <noviakhoirunnisa05@gmail.com>
2026-05-30 12:00:34 +0700

    menambahkan fitur beasiswa

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,27 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+use App\Http\Controllers\ScholarshipController;
+use App\Http\Controllers\BookmarkController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\FaqController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profile', [AuthController::class, 'profile']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/scholarships', [ScholarshipController::class, 'index']);
+    Route::get('/scholarships/{id}', [ScholarshipController::class, 'show']);
+    Route::post('/scholarships', [ScholarshipController::class, 'store']);
+    Route::put('/scholarships/{id}', [ScholarshipController::class, 'update']);
+    Route::delete('/scholarships/{id}', [ScholarshipController::class, 'destroy']);
+
+    Route::get('/bookmarks', [BookmarkController::class, 'index']);
+    Route::post('/bookmarks', [BookmarkController::class, 'store']);
+    Route::delete('/bookmarks/{id}', [BookmarkController::class, 'destroy']);
+
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::get('/articles/{id}', [ArticleController::class, 'show']);
+    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::put('/articles/{id}', [ArticleController::class, 'update']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+
+    Route::get('/faqs', [FaqController::class, 'index']);
+    Route::post('/faqs', [FaqController::class, 'store']);
+    Route::put('/faqs/{id}', [FaqController::class, 'update']);
+    Route::delete('/faqs/{id}', [FaqController::class, 'destroy']);
 });
