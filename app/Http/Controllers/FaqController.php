34213d9cf3c0393f9899c1e311462c7ef8c2a308
noviakhoirunnisa05-star <noviakhoirunnisa05@gmail.com>
2026-05-31 13:36:37 +0700

    menyelesaikan conflict controller dan route

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
 
-=======
-<<<<<<< HEAD
-<?php
-
-=======
->>>>>>> origin/main
->>>>>>> main
 namespace App\Http\Controllers;
 
 use App\Models\Faq;
@@ -42,21 +34,11 @@ class FaqController extends Controller
     public function update(Request $request, $id)
     {
         $faq = Faq::findOrFail($id);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> main
 
         $faq->update([
             'pertanyaan' => $request->pertanyaan,
             'jawaban' => $request->jawaban,
         ]);
-<<<<<<< HEAD
-=======
-=======
-        $faq->update($request->all());
->>>>>>> origin/main
->>>>>>> main
 
         return response()->json([
             'message' => 'FAQ berhasil diperbarui',
@@ -66,17 +48,8 @@ class FaqController extends Controller
 
     public function destroy($id)
     {
-<<<<<<< HEAD
         $faq = Faq::findOrFail($id);
         $faq->delete();
-=======
-<<<<<<< HEAD
-        $faq = Faq::findOrFail($id);
-        $faq->delete();
-=======
-        Faq::findOrFail($id)->delete();
->>>>>>> origin/main
->>>>>>> main
 
         return response()->json([
             'message' => 'FAQ berhasil dihapus'
